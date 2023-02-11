f21bb1128c6f0f7a880c933326a22ea579b5bfa6
Vivek Kumar <110529479+Vivek-1012@users.noreply.github.com>
2023-02-12 03:44:44 +0530

    doc
    files added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>VKStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="/" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://instagram.com/_im_v_i_v_e_k_" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/Vivek-1012" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
